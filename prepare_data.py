60363a3e000b90d208adf2b7f8317390a35a7c06
Jackson2706 <dung27062002@gmail.com>
2023-11-17 15:00:23 +0700

    avoid xml file not inlcude bnbbox

---
+++ prepare_data.py
@@ -0,0 +1,22 @@
+import os
+import xml.etree.ElementTree as ET
+from glob import glob
+
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="SHqXH9Hthjol3EotLlHd")
+project = rf.workspace("ctarg").project("ars")
+dataset = project.version(1).download("voc")
+
+data_dir = "./ARS-1"
+for phase in ["train", "valid", "test"]:
+    image_path_list = sorted(glob(data_dir + "/" + phase + "/*.jpg"))
+    xml_path_list = sorted(glob(data_dir + "/" + phase + "/*.xml"))
+
+    for image_path, xml_path in zip(image_path_list, xml_path_list):
+        tree = ET.parse(xml_path)
+        root = tree.getroot()
+        if root.find("object/bndbox") is None:
+            print(image_path, xml_path)
+            os.remove(xml_path)
+            os.remove(image_path)
